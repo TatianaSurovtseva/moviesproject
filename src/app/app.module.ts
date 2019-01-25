3591753a25d6cb30ab350f17a3055173b1f77274
tanjas <tanja.surovtseva@vertimart.nl>
2019-01-25 16:14:33 +0100

    another try

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {routing} from './app.routing';
 import { AppComponent } from './app.component';
+import { NavbarComponent} from './components/navbar/navbar.component'
+import { AboutComponent }from './components/about/about.component';
+import { SearchComponent }from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
   ],
-  imports: [
+  imports: [routing,
     BrowserModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
